6043e690b9fe85f5338b25ff18ab99cc2d567e86
RuijianAn <992003469@qq.com>
2017-08-29 08:38:09 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,54 +9,22 @@
 #define N_V 1024
 
 
-struct sma{
-    int m[N_V/NUM_THREADS][N_V];
-    int id;
-};
 
 int flag = 1;
 int mask[N_V];
+
+int m[N_V][N_V];
+int d[N_V];
+
 int mask1[N_V];
 
 
-int d[N_V];
 
 
 
 
-
-
-
-
-
-/* Work-efficient multithread CPU version */
-
-void *helper(void *c){
-    int taskid;
-    
-    struct sma *my_data;
-    my_data = (struct sma *) c;
-    taskid = my_data->id;
-    
-    int first, last;
-    
-    first = (taskid * N_V)/NUM_THREADS;
-    last = first + N_V/NUM_THREADS;
-    
-    int i, j;
-    
-    for (i=first; i<last; i++)
-        for (j=0; j<N_V; j++){
-            if (mask[j] == 1){
-                if (d[i] == INT_MAX && d[i] > d[j] + my_data->m[j][i])
-                    d[i] = d[j] + my_data->m[j][i];
-                    mask1[i] = 1;
-                    flag = 1;
-            }
-        }
-    free(c);
-    pthread_exit(NULL);
-}
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_barrier_t barrier;
 
 void swap(int *a, int *b)
 {
@@ -68,10 +36,48 @@ void swap(int *a, int *b)
 }
 
 
-int main(){
-    struct sma *a = NULL;
-    int i, j, k, p;
+
+void *helper(void *arg){
+    int taskid;
+
+    taskid = (int) arg;
     
+    int first, last;
+    
+    first = (taskid * N_V)/NUM_THREADS;
+    last = first + N_V/NUM_THREADS;
+    
+    int i, j, k;
+    for (k=0; k<N_V; k++){
+    for (i=first; i<last; i++)
+        for (j=0; j<N_V; j++){
+            if (mask[j] == 1){
+                pthread_mutex_lock(&mutex);
+                if (d[i] == INT_MAX && d[i] > d[j] + m[j][i])
+                    d[i] = d[j] + m[j][i];
+                    mask1[i] = 1;
+                    flag = 1;
+                pthread_mutex_unlock(&mutex);
+            }
+            
+        }
+        pthread_barrier_wait(&barrier);
+        if (flag == 0)
+            break;
+        flag = 0;
+        
+        for (i=0; i<N_V; i++)
+            swap(&mask[i], &mask1[i]);
+    }
+    free(arg);
+    pthread_exit(NULL);
+}
+
+
+int main(){
+    int j, p;
+    long i;
+    clock_t begin = clock();
     for (i=0; i<N_V; i++)
     {
         d[i] = INT_MAX;
@@ -81,104 +87,99 @@ int main(){
     d[0] = 0;
     mask[0] = 1;
     
-    pthread_t thread[NUM_THREADS];
-    
-    clock_t begin = clock();
-    flag = 1;
-    
-    /* Outer loop */
-    for (k=0; k<N_V; k++){
-        /* Create multiple threads */
-        for (i=0; i<NUM_THREADS; i++)
-            a = (struct sma *) malloc(sizeof(struct sma));
-            for (p=0; p<N_V/NUM_THREADS; p++)
-                for (j=0; j<N_V; j++)
-                {
-                    a->m[p][j] = rand() % 40;
-                }
-            a->id = i;
-            pthread_create(&thread[i], NULL, helper, (void *) a);
-        
-        /* Synchronization */
-        for (i=0; i<NUM_THREADS; i++)
-            pthread_join(thread[i], NULL);
-        
-        
-        if (flag == 0)
-            break;
-        flag = 0;
-        
-        for (i=0; i<N_V; i++)
-            swap(&mask[i], &mask1[i]);
+    for (p=0; p<N_V; p++)
+    {
+        for (j=0; j<N_V; j++)
+        {
+            m[p][j] = rand() % 40;
+        }
     }
     
+    
+    pthread_t thread[NUM_THREADS];
+    
+    
+    flag = 1;
+    for (i=0; i<NUM_THREADS; i++){
+        pthread_create(&thread[i], NULL, helper, (void *) i);
+    }
     
     clock_t end = clock();
     
     double time = (double)(end - begin)/CLOCKS_PER_SEC;
-    
+
     
     printf("%f\n", time);
     
     return 0;
-   
+    
+    
     
     
 }
 
-/* Work inefficent version */
-void *helper(void *c){
+
+void *helper(void *arg){
     int taskid;
-    struct sma *my_data;
-    my_data = (struct sma *) c;
-    taskid = my_data->id;
+    taskid = (int) arg;
     
     int first, last;
     
     first = (taskid * N_V)/NUM_THREADS;
     last = first + N_V/NUM_THREADS;
     
-    int i, j;
+    int i, j, k;
     
-    for (i=first; i<last; i++)
+    for (k=0; k<N_V; k++){
         for (j=0; j<N_V; j++)
-            if (d[i] == INT_MAX && d[i] > d[j] + my_data->m[j][i])
-                d[i] = d[j] + my_data->m[j][i];
-    free(c);
+        {
+         for (i=first; i<last; i++){
+            pthread_mutex_lock(&mutex);
+            if (d[i] == INT_MAX && d[i] > d[j] + m[j][i])
+                d[i] = d[j] + m[j][i];
+            pthread_mutex_unlock(&mutex);
+         }
+        }
+        pthread_barrier_wait(&barrier);
+    }
+    free(arg);
     pthread_exit(NULL);
+
 }
 
 
+
+    
 int main(){
-    struct sma *a = NULL;
-    int i, j, k, p;
+    int  j, p;
+    long i;
+    
+    
+    clock_t begin = clock();
     
     for (i=0; i<N_V; i++)
     {
         d[i] = INT_MAX;
     }
-    
     d[0] = 0;
-    pthread_t thread[NUM_THREADS];
-        clock_t begin = clock();
     
-    for (k=0; k<N_V; k++){
-        for (i=0; i<NUM_THREADS; i++){
-            a = (struct sma *) malloc(sizeof(struct sma));
-            for (p=0; p<N_V/NUM_THREADS; p++)
-                for (j=0; j<N_V; j++)
-                {
-                    a->m[p][j] = rand() % 40;
-                }
-            a->id = i;
-            pthread_create(&thread[i], NULL, helper, (void *) a);
+    for (p=0; p<N_V; p++)
+    {
+        for (j=0; j<N_V; j++)
+        {
+            m[p][j] = rand() % 40;
         }
-        for (i=0; i<NUM_THREADS; i++)
-            pthread_join(thread[i], NULL);
-    
     }
     
     
+    pthread_t thread[NUM_THREADS];
+    pthread_barrier_init(&barrier, NULL, NUM_THREADS);
+    
+    for (i=0; i<NUM_THREADS; i++){
+        pthread_create(&thread[i], NULL, helper, (void *) i);
+    }
+
+
     clock_t end = clock();
     
     double time = (double)(end - begin)/CLOCKS_PER_SEC;
@@ -187,9 +188,6 @@ int main(){
     printf("%f\n", time);
     
     return 0;
-    
-    
-    
     
 }
 
